f90d6fc6002958f574d8991fe76763abd2118fed
Ruslan Curbanov <rucub100@hhu.de>
2021-01-26 19:34:11 +0100

    improve header behavior and style

--- js/header-hide-onscroll.js
+++ js/header-hide-onscroll.js
@@ -4,8 +4,12 @@ window.onscroll = function () {
   const currentPageYOffset = window.pageYOffset;
   if (prevPageYOffset > currentPageYOffset) {
     document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-100px";
+    document.getElementById("header").style.boxShadow =
+      "0px 2px 4px rgba(0, 0, 0, 0.1)";
+  } else if (currentPageYOffset > 200) {
+    const offsetHeight = document.getElementById("header").offsetHeight;
+    document.getElementById("header").style.top = "-" + offsetHeight + "px";
+    document.getElementById("header").style.boxShadow = "none";
   }
   prevPageYOffset = currentPageYOffset;
 };
