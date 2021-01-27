62874606b4ecec54772534ff685d226936820535
Ruslan Curbanov <rucub100@hhu.de>
2021-01-27 23:23:11 +0100

    add sitemap

--- js/header-hide-onscroll.js
+++ js/header-hide-onscroll.js
@@ -6,7 +6,7 @@ window.onscroll = function () {
     document.getElementById("header").style.top = "0";
     document.getElementById("header").style.boxShadow =
       "0px 2px 4px rgba(0, 0, 0, 0.1)";
-  } else if (currentPageYOffset > 200) {
+  } else if (currentPageYOffset > 400) {
     const offsetHeight = document.getElementById("header").offsetHeight;
     document.getElementById("header").style.top = "-" + offsetHeight + "px";
     document.getElementById("header").style.boxShadow = "none";
